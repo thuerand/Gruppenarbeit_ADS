4c1f25a079bc3870e4b30142335d2a2ecefb4fe6
Salzmee <thuerand@students.zhaw.ch>
2024-04-16 21:21:17 +0200

    changed_mapPopup

--- HQ_visualized.py
+++ HQ_visualized.py
@@ -7,9 +7,12 @@ import folium
 import warnings
 
 # Ignore specific pandas UserWarning about SQLAlchemy connectable
-warnings.filterwarnings('ignore', category=UserWarning, message='.*pandas only supports SQLAlchemy connectable.*')
+warnings.filterwarnings('ignore', category=UserWarning,
+                        message='.*pandas only supports SQLAlchemy connectable.*')
 
-#Function to fetch coordinates from OpenCage Geocoder API
+# Function to fetch coordinates from OpenCage Geocoder API
+
+
 def fetch_coordinates(api_key, address):
     """Fetch coordinates from OpenCage Geocoder API."""
     base_url = "https://api.opencagedata.com/geocode/v1/json"
@@ -18,7 +21,7 @@ def fetch_coordinates(api_key, address):
         "key": api_key,
         "limit": 1
     }
-    response = requests.get(base_url, params=params)
+    response = requests.get(base_url, params=params, timeout=10)
     data = response.json()
     if data['results']:
         latitude = data['results'][0]['geometry']['lat']
@@ -26,9 +29,11 @@ def fetch_coordinates(api_key, address):
         return latitude, longitude
     return None, None
 
-#Function to update the database and CSV file with coordinates
+# Function to update the database and CSV file with coordinates
+
+
 def update_database_and_csv_with_coordinates():
-    #Update both the database and CSV file with geographic coordinates."""
+    # Update both the database and CSV file with geographic coordinates."""
     csv_file_path = 'Data_cryptonews/HQ_newsagency.csv'
 
     df = pd.read_csv(csv_file_path)  # Load the CSV file into a DataFrame
@@ -44,7 +49,8 @@ def update_database_and_csv_with_coordinates():
         api_key = '77ace187bc104bcfa94efa947fd84326'
 
         # Select domains that are missing coordinates
-        cursor.execute("SELECT Domain, hq_location FROM HQ_newagency WHERE latitude IS NULL OR longitude IS NULL")
+        cursor.execute(
+            "SELECT Domain, hq_location FROM HQ_newagency WHERE latitude IS NULL OR longitude IS NULL")
         rows = cursor.fetchall()
 
         # Mapping domains to hq_location for API calls
@@ -54,7 +60,8 @@ def update_database_and_csv_with_coordinates():
         for index, row in df.iterrows():
             if row['Domain'] in domain_to_hq:
                 if pd.isna(row['latitude']) or pd.isna(row['longitude']):
-                    latitude, longitude = fetch_coordinates(api_key, domain_to_hq[row['Domain']])
+                    latitude, longitude = fetch_coordinates(
+                        api_key, domain_to_hq[row['Domain']])
                     if latitude is not None and longitude is not None:
                         df.at[index, 'latitude'] = latitude
                         df.at[index, 'longitude'] = longitude
@@ -65,9 +72,11 @@ def update_database_and_csv_with_coordinates():
 
                     # Update the database
                     update_query = """UPDATE HQ_newagency SET latitude = %s, longitude = %s WHERE Domain = %s"""
-                    cursor.execute(update_query, (latitude, longitude, row['Domain']))
+                    cursor.execute(
+                        update_query, (latitude, longitude, row['Domain']))
                     connection.commit()
-                    print(f"Updated {row['Domain']} with lat: {latitude}, lng: {longitude}")
+                    print(
+                        f"Updated {row['Domain']} with lat: {latitude}, lng: {longitude}")
 
         # Save the updated DataFrame back to the CSV
         df.to_csv(csv_file_path, index=False)
@@ -82,20 +91,23 @@ def update_database_and_csv_with_coordinates():
             print("MySQL connection is closed")
 
 # Example usage
-#update_database_and_csv_with_coordinates()
+# update_database_and_csv_with_coordinates()
 
-#Function to fetch data from the MySQL database and create an interactive map using Folium
+# Function to fetch data from the MySQL database and create an interactive map using Folium
+
+
 def fetch_and_create_map():
     """Fetch data from the MySQL database and create an interactive map using Folium, grouping multiple domains at the same location."""
     try:
         # Connect to the MySQL database
-        conn = mysql.connector.connect(host='localhost', user='myuser', password='mypassword', database='mydatabase')
+        conn = mysql.connector.connect(
+            host='localhost', user='myuser', password='mypassword', database='mydatabase')
         query = "SELECT Domain, hq_location, latitude, longitude FROM HQ_newagency WHERE latitude IS NOT NULL AND longitude IS NOT NULL"
         data = pd.read_sql(query, conn)
-        
+
         # Ensure the connection is closed after fetching the data
         conn.close()
-        
+
         # Check if data is empty
         if data.empty:
             print("No data available to display on the map.")
@@ -114,10 +126,11 @@ def fetch_and_create_map():
 
         # Add markers to the map for each group
         for (lat, lon, hq), group in grouped:
-            domains_list = '<br>'.join(f"{row['Domain']}" for _, row in group.iterrows())
+            domains_list = '<br>'.join(
+                f" - {row['Domain']}" for _, row in group.iterrows())
             popup_text = f"<strong>HQ: {hq}</strong><br>Domains:<br>{domains_list}"
             popup = folium.Popup(popup_text, max_width=300)  # Set a larger width for the popup
-            folium.Marker([lat, lon], popup=popup_text).add_to(map)
+            folium.Marker([lat, lon], popup=popup).add_to(map)
 
         # Save the map as an HTML file
         map.save('result/hq_locations_map.html')
@@ -131,4 +144,4 @@ def fetch_and_create_map():
         print(f"An error occurred: {e}")
 
 # Example usage
-#fetch_and_create_map()
+fetch_and_create_map()
